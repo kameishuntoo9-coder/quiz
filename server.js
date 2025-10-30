8a9704a54a6b394dc980df3271c4ed40c2c634a7
kameishuntoo9-coder <kamei.shunto.o9@elms.hokudai.ac.jp>
2025-10-30 21:23:59 +0900

    Create package.json for multiplayer quiz app
    Added package.json with app metadata and dependencies.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-package.json
-server.js
-questions.json
-public/index.html
-public/client.js
-public/styles.css
+{
+  "name": "multiplayer-quiz-app",
+  "version": "1.0.0",
+  "description": "Multiplayer quiz (rooms + MCQ + ordering + video buzzer) with Socket.IO",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "nanoid": "^5.0.7",
+    "socket.io": "^4.7.5"
+  }
+}
